1f55474160276ec4a25da3b87a2f3b012ac68c3e
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-11-08 18:16:23 +0100

    Movies Ram Memory Local layer finished

---
+++ app/src/main/java/com/example/moviesapp/data/local/memory/MoviesMemoryLocalDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.moviesapp.data.local.memory
+
+import com.example.app.commons.MemoryDataStore
+import com.example.moviesapp.data.local.MoviesLocalDataRepository
+import com.example.moviesapp.domain.Movie
+
+class MoviesMemoryLocalDataSource (val memoryDataStore: MemoryDataStore<Movie>): MoviesLocalDataRepository{
+
+    override fun getAllMovies(): List<Movie> {
+        return memoryDataStore.getAll()
+    }
+
+    override fun getMovieById(id: String): Movie? {
+        return memoryDataStore.getById(id)
+    }
+
+    override fun saveMovies(list: List<Movie>) {
+        list.forEach {
+            memoryDataStore.put(it.id, it)
+        }
+    }
+
+}
