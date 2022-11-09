8b9ce34075e7d93cd3045f601cfffd9875748502
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-11-09 17:08:39 +0100

    Movies Firebase Remote Data Source Finished

--- app/src/main/java/com/example/moviesapp/data/remote/retrofit/MovieRetrofitRemoteDataSource.kt
+++ app/src/main/java/com/example/moviesapp/data/remote/retrofit/MovieRetrofitRemoteDataSource.kt
@@ -5,13 +5,13 @@ import com.example.moviesapp.domain.Movie
 
 class MovieRetrofitRemoteDataSource (val client: ApiClient) : MoviesRemoteDataRepository{
 
-    override fun getAllMovies(): List<Movie> {
+    override suspend fun getAllMovies(): List<Movie> {
         return client.getAllMovies().map {
             it.toDomain()
         }
     }
 
-    override fun getMovieById(id: String): Movie? {
+    override suspend fun getMovieById(id: String): Movie? {
         return client.getMovieById(id)?.toDomain()
     }
 
